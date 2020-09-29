f01c242bfd3c9e8dfba5733a0a4f145a8caa88b7
devankit01 <devankitgupta01@gmail.com>
2020-09-29 07:35:51 -0700

    first commit

--- Django_Main_App/settings.py
+++ Django_Main_App/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5mth_9u3)870gqyp@yq$+3&uqv3ztkvs0id1*3x&18!ms4pvaj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
